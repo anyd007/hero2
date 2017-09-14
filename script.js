a145a2034a2efaa40d7d996dbbe67cecba84a0ea
Andrzej <andy-26@wp.pl>
2017-09-14 13:23:17 +0200

    zmiany hero

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ var slideSwitch = function(slideTo) {
 
     $active.addClass("last-active");
 
-    // added “slideTo” variable to allow transition to a selected slide
-    // defaults to null, but if it’s >= 0, it will use this index for “$next”
     slideTo = ( slideTo+1 )? slideTo : null;
     if ( slideTo !== null ) {
         $next = $(".bg IMG").eq(slideTo);
@@ -24,9 +22,9 @@ var slideSwitch = function(slideTo) {
         });
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-    // hide all images except first to avoid initial flicker
+
     $('.bg IMG').css({opacity: 0.0});
     $('.bg IMG:first').css({opacity: 1.0});
 
@@ -36,12 +34,34 @@ $(document).ready(function(){
     // create buttons to move to specific slide
     var $slideButtons = $('.ssPushContainer ul li');
 
-    $slideButtons.click(function(){
+    $slideButtons.click(function () {
         // stop the slideshow, to keep it from trying to overlap our transition
         // clearInterval(playSlideshow);
 
         // call the function using the index of the clicked button
-        slideSwitch( $slideButtons.index(this) );
+        slideSwitch($slideButtons.index(this));
 
     });
+    // znikające butony
+    $("#btn1").click(function(){
+        $('#btn1').hide(500);
+        $('#btn2').show(500);
+        $('#btn3').show(500);
+     });
+    $('#btn2').click(function () {
+        $('#btn2').hide(500);
+        $('#btn1').show(500);
+        $('#btn3').show(500);
+
+    });
+    $('#btn3').click(function () {
+        $('#btn3').hide(500);
+        $('#btn2').show(500);
+        $('#btn1').show(500);
+
+    })
+    // znikające butony koniec
 });
+
+
+
